22bc0f472519068461467c051fd4426fc122a5ac
Chien-Te Liu <ufoderek@gmail.com>
2015-10-10 21:49:26 +0800

    Replace tab with spaces in makefiles

--- Defs.mk
+++ Defs.mk
@@ -1,57 +1,57 @@
-CPP				:=	g++
+CPP             :=  g++
 OS              :=  $(shell uname -s)
 
-OPT_FLAG		:=	-O2 -march=native -fomit-frame-pointer -fvisibility-inlines-hidden -pipe
-MVP_FLAG		:=	-DSC_INCLUDE_DYNAMIC_PROCESSES
-FUCKGCC_FLAG	:=	-fpermissive -Wno-write-strings -w
-RELEASE_FLAG	:=	$(OPT_FLAG) $(MVP_FLAG) $(FUCKGCC_FLAG) $(COMMON_FLAG) -DRELEASE
-DEBUG_FLAG		:=	$(OPT_FLAG) $(MVP_FLAG) $(FUCKGCC_FLAG) $(COMMON_FLAG) -DINST_ASSERT -DCPU_ASSERT
+OPT_FLAG        :=  -O2 -march=native -fomit-frame-pointer -fvisibility-inlines-hidden -pipe
+MVP_FLAG        :=  -DSC_INCLUDE_DYNAMIC_PROCESSES
+FUCKGCC_FLAG    :=  -fpermissive -Wno-write-strings -w
+RELEASE_FLAG    :=  $(OPT_FLAG) $(MVP_FLAG) $(FUCKGCC_FLAG) $(COMMON_FLAG) -DRELEASE
+DEBUG_FLAG      :=  $(OPT_FLAG) $(MVP_FLAG) $(FUCKGCC_FLAG) $(COMMON_FLAG) -DINST_ASSERT -DCPU_ASSERT
 
-STRIP			:=	strip
+STRIP           :=  strip
 ifeq ($(OS),Darwin)
-STRIP_FLAG		:=	-S
+STRIP_FLAG      :=  -S
 endif
 ifeq ($(OS),Linux)
-STRIP_FLAG		:=	-s
+STRIP_FLAG      :=  -s
 endif
 
 TOOLCHAIN_BASE  :=  /Volumes/work/wk/toolchain
-SC_INC			:=	-I$(TOOLCHAIN_BASE)/systemc-2.3.1/include
-SC_LIB			:=	-L$(TOOLCHAIN_BASE)/systemc-2.3.1/lib-macosx64
+SC_INC          :=  -I$(TOOLCHAIN_BASE)/systemc-2.3.1/include
+SC_LIB          :=  -L$(TOOLCHAIN_BASE)/systemc-2.3.1/lib-macosx64
 BOOST_INC       :=  -I/usr/local/Cellar/boost/1.58.0/include
 BOOST_LIB       :=  -I/usr/local/Cellar/boost/1.58.0/lib
-INC				:=	$(SC_INC) $(BOOST_INC)
-LIB				:=	$(SC_LIB) $(BOOST_LIB) -lsystemc
+INC             :=  $(SC_INC) $(BOOST_INC)
+LIB             :=  $(SC_LIB) $(BOOST_LIB) -lsystemc
 
-platform		:=
-target			:=
-sub_dirs		:=	
-CPP_FLAG		:=
+platform        :=
+target          :=
+sub_dirs        :=  
+CPP_FLAG        :=
 
 ifeq "$(MAKECMDGOALS)" "pb"
-platform		:=	pb
-target			:=	mvp_realviewpb_armv7a
-sub_dirs		+=	armv7a armv7a_inst realviewpb_top hw sys tlm_bus armv7a_tlb 
-CPPFLAG			+=	$(RELEASE_FLAG)
+platform        :=  pb
+target          :=  mvp_realviewpb_armv7a
+sub_dirs        +=  armv7a armv7a_inst realviewpb_top hw sys tlm_bus armv7a_tlb 
+CPPFLAG         +=  $(RELEASE_FLAG)
 endif
 
 ifeq "$(MAKECMDGOALS)" "pbd"
-platform		:=	pbd
-target			:=	mvp_realviewpb_armv7a_debug
-sub_dirs		+=	armv7a armv7a_inst realviewpb_top hw sys tlm_bus armv7a_tlb
-CPPFLAG			+=	$(DEBUG_FLAG)
+platform        :=  pbd
+target          :=  mvp_realviewpb_armv7a_debug
+sub_dirs        +=  armv7a armv7a_inst realviewpb_top hw sys tlm_bus armv7a_tlb
+CPPFLAG         +=  $(DEBUG_FLAG)
 endif
 
 ifeq "$(MAKECMDGOALS)" "vb"
-platform		:=	vb
-target			:=	mvp_meteorvb_armv7a
-sub_dirs		+=	armv7a armv7a_inst meteorvb_top hw sys tlm_bus
-CPPFLAG			+=	$(RELEASE_FLAG)
+platform        :=  vb
+target          :=  mvp_meteorvb_armv7a
+sub_dirs        +=  armv7a armv7a_inst meteorvb_top hw sys tlm_bus
+CPPFLAG         +=  $(RELEASE_FLAG)
 endif
 
 ifeq "$(MAKECMDGOALS)" "vbd"
-platform		:=	vbd
-target			:=	mvp_meteorvb_armv7a_debug
-sub_dirs		+=	armv7a armv7a_inst meteorvb_top hw sys tlm_bus
-CPPFLAG			+=	$(DEBUG_FLAG)
+platform        :=  vbd
+target          :=  mvp_meteorvb_armv7a_debug
+sub_dirs        +=  armv7a armv7a_inst meteorvb_top hw sys tlm_bus
+CPPFLAG         +=  $(DEBUG_FLAG)
 endif
--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 # include user defined parameters
 include Defs.mk
 
-bin_dir				:=	bin
-target_bin			:=	$(bin_dir)/$(target)
-build_dir			:=	builds/$(target)/
+bin_dir             :=  bin
+target_bin          :=  $(bin_dir)/$(target)
+build_dir           :=  builds/$(target)/
 
-src_files			:=	$(shell find $(sub_dirs) -name '*.cpp')
+src_files           :=  $(shell find $(sub_dirs) -name '*.cpp')
 
-obj_files			:=	$(notdir $(src_files:%.cpp=%.o))
-full_obj_files		:=	$(addprefix $(build_dir), $(obj_files))
+obj_files           :=  $(notdir $(src_files:%.cpp=%.o))
+full_obj_files      :=  $(addprefix $(build_dir), $(obj_files))
 
-dep_files			:=	$(subst .o,.d,$(full_obj_files))
+dep_files           :=  $(subst .o,.d,$(full_obj_files))
 
-INC					+=	$(addprefix -I./, $(addsuffix /include, $(sub_dirs)))
+INC                 +=  $(addprefix -I./, $(addsuffix /include, $(sub_dirs)))
 
-create-build-dir	:=	$(shell test -d $(build_dir) || mkdir -p $(build_dir))
-create-binary-dir	:=	$(shell test -d $(bin_dir) || mkdir -p $(bin_dir))
+create-build-dir    :=  $(shell test -d $(build_dir) || mkdir -p $(build_dir))
+create-binary-dir   :=  $(shell test -d $(bin_dir) || mkdir -p $(bin_dir))
 
 vpath %.cpp $(sub_dirs)
 
@@ -23,13 +23,13 @@ vpath %.cpp $(sub_dirs)
 $(platform): $(target_bin)
 
 $(target_bin): $(full_obj_files)
-	@echo	'	LD $@'
+	@echo   '    LD $@'
 	@$(CPP) $(CPPFLAG) $(INC) $^ -o $@ $(LIB)
-	@echo	'	STRIP $@'
+	@echo   '    STRIP $@'
 	@$(STRIP) $(STRIP_FLAG) $@
 
 $(build_dir)%.o: %.cpp
-	@echo '	CPP $<'
+	@echo '    CPP $<'
 	@$(CPP) $(CPPFLAG) $(INC) -M -MF $(@:.o=.d) -MP -MT $@ $<
 	@$(CPP) $(CPPFLAG) $(INC) $< -c -o $@
 
